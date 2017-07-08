c9842ed37108bea7bbf3473b072d5db1b975ded3
John <iplanetcn@gmail.com>
2017-07-09 04:16:08 +0800

    add two blank line

--- app/src/main/java/com/phenix/ci/MainActivity.java
+++ app/src/main/java/com/phenix/ci/MainActivity.java
@@ -11,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ((TextView) findViewById(R.id.tv_message)).setText(R.string.master);
+
+
+
     }
 }
