a6091f36949ec9d319072aaaeef0ea618804e09f
John <iplanetcn@gmail.com>
2017-07-09 03:56:53 +0800

    change message to develop

--- app/src/main/java/com/phenix/ci/MainActivity.java
+++ app/src/main/java/com/phenix/ci/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ((TextView) findViewById(R.id.tv_message)).setText(R.string.master);
+        ((TextView) findViewById(R.id.tv_message)).setText(R.string.develop);
     }
 }
