bf8f6fc77523ac2dd6c7b4241fb34796efbefbe4
John <iplanetcn@gmail.com>
2017-07-14 15:27:59 +0800

    delete message#4.

--- app/src/main/java/com/phenix/ci/MainActivity.java
+++ app/src/main/java/com/phenix/ci/MainActivity.java
@@ -18,7 +18,6 @@ public class MainActivity extends AppCompatActivity {
             Log.d(TAG, "message#1");
             Log.d(TAG, "message#2");
             Log.d(TAG, "message#3");
-            Log.d(TAG, "message#4");
         }
     }
 }
